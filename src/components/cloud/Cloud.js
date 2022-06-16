5d93190794dd4e6b50e1d1e4435248a4a509cd48
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-16 20:26:23 +0300

    Security

--- src/components/cloud/Cloud.js
+++ src/components/cloud/Cloud.js
@@ -8,6 +8,12 @@ const Cloud = () => {
           <h2>
             <span>Cloud</span>Security
           </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            nostrum itaque eum iure iste ipsa repellendus neque quod fugit
+            pariatur deserunt, quae nesciunt eius doloribus ipsam quia porro
+            obcaecati sapiente. Lorem ipsum dolor sit amet, consectetur
+          </p>
         </div>
       </div>
     </div>
