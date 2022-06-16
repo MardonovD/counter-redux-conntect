5b0aff8a21a86dff383de34687ea057454bd6099
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-16 17:31:34 +0300

    navbar ul

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,6 +9,14 @@ const Navbar = () => {
           <SiDatabricks classNameicon />
           <h1>Secured</h1>
         </div>
+
+        <ul className="nav-menu">
+          <li>Home</li>
+          <li>Recovery</li>
+          <li>Cloud </li>
+          <li>Contact</li>
+          <button>Sign in</button>
+        </ul>
       </div>
     </div>
   );
