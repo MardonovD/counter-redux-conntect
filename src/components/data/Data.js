fea1d8e14bc9e2dba2391f4dc9d8d01ac902008d
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-16 19:51:32 +0300

    Sign Up

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -1,14 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import "./DataStyle.css";
 
 const Data = () => {
   return (
-    <div className='data'>
-      
+    <div className="data">
+      <div className="container">
+        <div className="content">
+          <h2>
+            <span>Data</span>Recovery
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            nostrum itaque eum iure iste ipsa repellendus neque quod fugit
+            pariatur deserunt, quae nesciunt eius doloribus ipsam quia porro
+            obcaecati sapiente.
+          </p>
+          <button>Sign Up</button>
+        </div>
+      </div>
     </div>
   );
 };
-
-
 
 export default Data;
