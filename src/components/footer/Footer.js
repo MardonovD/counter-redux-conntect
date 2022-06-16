a1d97868a257e434240739eb65b7564b3ffda78f
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-16 20:49:42 +0300

    Footer statr

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./FooterStyle.css";
 
 const Footer = () => {
-  return <div className="footer"></div>;
+  return <div className="footer">
+    <div className="container">
+      
+    </div>
+  </div>;
 };
 
 export default Footer;
