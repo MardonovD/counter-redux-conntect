b9f16699ed64ea61ca0b03a5754dc04b92c42e98
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-16 20:47:26 +0300

    Footer statr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Cloud from "./components/cloud/Cloud";
 import Data from "./components/data/Data";
+import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Data />
       <Cloud />
+      <Footer />
     </>
   );
 }
