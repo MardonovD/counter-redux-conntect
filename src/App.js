1a0781af3fb0b5cb3021cea3d98075e7a64e2e0d
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-16 12:13:34 +0300

    main page text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import Hero from "./components/hero/Hero";
 function App() {
-  return <>efkledvndklfn</>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
 
 export default App;
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./HeroStyle.css";
 const Hero = () => {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="content">
+          <h1>Data</h1>
+          <h1>Secured</h1>
+          <h1>Protection</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
